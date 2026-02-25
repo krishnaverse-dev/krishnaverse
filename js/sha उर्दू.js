b39e7c24894ecf081368231d2625e61dc11a1b4e
vaidik map and research <bramhaatma@gmail.com>
2026-02-25 14:24:06 +0545

    Update sha उर्दू.js

--- js/sha उर्दू.js
+++ js/sha उर्दू.js
@@ -1,13 +1,13 @@
 function shareSite() {
-        if (navigator.share) {
-            navigator.share({
-                title: 'کرشنورس',
-                text: 'ویدک سفر',
-                url: window.location.href
-            }).catch(console.error);
-        } else {
-            navigator.clipboard.writeText(window.location.href)
-                .then(() => alert("لنک کلپ بورڈ پر کاپی ہو گیا!"))
-                .catch(() => alert("لنک کاپی کرنے میں ناکام۔"));
-        }
+    const url = "https://krishnaverse-dev.github.io/krishnaverse/";
+
+    if (navigator.share) {
+        navigator.share({
+            url: url
+        }).catch(console.error);
+    } else {
+        navigator.clipboard.writeText(url)
+            .then(() => alert("Link copied to clipboard!"))
+            .catch(() => alert("Failed to copy link."));
     }
+}
