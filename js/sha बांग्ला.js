0c01630c4678b8ae5b729ff657e544c02f899321
vaidik map and research <bramhaatma@gmail.com>
2026-02-25 14:25:45 +0545

    Update sha बांग्ला.js

--- js/sha बांग्ला.js
+++ js/sha बांग्ला.js
@@ -1,13 +1,13 @@
 function shareSite() {
-        if (navigator.share) {
-            navigator.share({
-                title: 'কৃষ্ণবর্ষ',
-                text: 'বৈদিক যাত্রা',
-                url: window.location.href
-            }).catch(console.error);
-        } else {
-            navigator.clipboard.writeText(window.location.href)
-                .then(() => alert("লিঙ্ক ক্লিপবোর্ডে কপি হয়েছে!"))
-                .catch(() => alert("লিঙ্ক কপি করতে ব্যর্থ।"));
-        }
+    const url = "https://krishnaverse-dev.github.io/krishnaverse/";
+
+    if (navigator.share) {
+        navigator.share({
+            url: url
+        }).catch(console.error);
+    } else {
+        navigator.clipboard.writeText(url)
+            .then(() => alert("Link copied to clipboard!"))
+            .catch(() => alert("Failed to copy link."));
     }
+}
